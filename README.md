2e52591860d5c3f180e0b9a96dbec2fa54965f70
Bryan Geraldio <155304225+Geraldioo@users.noreply.github.com>
2024-05-02 20:38:17 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# [Bryan Geraldioo Portfolio](https://)
+
+🚀 Welcome to my latest portfolio project! 🚀
+
+This project is a showcase of my skills and projects, developed using HTML5, CSS3, JavaScript, and powered by React.js.
+
+## Let's Connect:
+
+Feel free to reach out via email at [bryangeraldioo@gmail.com](mailto:your.email@example.com) or connect with me on [LinkedIn](https://www.linkedin.com/in/bryan-geraldio-9599842b8/).
---
+++ src/data/constants.js
@@ -0,0 +1,322 @@
+export const Bio = {
+  name: "Bryan Geraldio",
+  image: "bryan.png",
+  roles: ["FullStack Developer", "Software Engineer", "Mobile Developer", "Programmer"],
+  description:
+    "To Kickstart My Career as a Software Developer, Utilizing My Strong Programming Skills and Eager to Learn From Experienced Professionals. My Goal is to Contribute Effectively to Projects, Innovate, and Grow as a Versatile Developer While Adding Value to the Organization",
+  github: "https://github.com/geraldioo",
+  resume:
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/bryan-geraldio-9599842b8/",
+  twitter: "https://twitter.com/bryangeraldioo",
+  insta: "https://www.instagram.com/bryangeraldioo/",
+  facebook: "https://www.facebook.com/bryangeraldioo/",
+};
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name: "JavaScript",
+        image: "JavaScript-logo.png",
+      },
+      {
+        name: "TypeScript",
+        image: "Typescript-logo.png",
+      },
+      {
+        name: "HTML",
+        image: "html5-logo.png",
+      },
+      {
+        name: "CSS",
+        image: "CSS3-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React",
+        image: "react-logo.png",
+      },
+      {
+        name: "React Native",
+        image: "react-native.jpg",
+      },
+      {
+        name: "NextJS",
+        image: "Next-logo.jpg",
+      },
+      {
+        name: "Vite",
+        image: "vite-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image: "bootstrap-logo.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image: "Tailwind-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "nodejs-logo.svg",
+      },
+      {
+        name: "Express",
+        image: "express-logo.png",
+      },
+      {
+        name: "PostgreSQL",
+        image: "postgre-logo.png",
+      },
+      {
+        name: "GraphQL",
+        image: "graphql-logo.png",
+      },
+      {
+        name: "MongoDB",
+        image: "mongo-logo.jpg",
+      },
+      {
+        name: "Firebase",
+        image: "firebase-logo.png",
+      },
+      {
+        name: "AWS",
+        image: "aws-logo.jpg",
+      },
+      {
+        name: "Sequelize",
+        image: "sequelieze-logo.png",
+      },
+      {
+        name: "JEST",
+        image: "jest-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Tools & Platforms",
+    skills: [
+      {
+        name: "Git",
+        image: "git.png",
+      },
+      {
+        name: "GitHub",
+        image: "GitHub-logo.png",
+      },
+      {
+        name: "Vercel",
+        image: "vercel-logo.png",
+      },
+      {
+        name: "Expo",
+        image: "expo-logo.png",
+      },
+      {
+        name: "VS Code",
+        image: "VSCode-log.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "DevOps & Fullstack Engineering Intern",
+    company: "Flipr Inovations Pvt. Ltd.",
+    date: "Aug 2023 - July 2023",
+    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+    skills: [
+      "Docker",
+      "Terraform",
+      "AWS",
+      "EC2",
+      "Portainer",
+      "Nginx",
+      "JavaScript",
+      "TypeScript",
+      "Node Js",
+      " Next Js",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+    role: "Frontend Engineer Intern",
+    company: "Neurobit",
+    date: "June 2023 - Present",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Fullstack Externship",
+    company: "Flipr",
+    date: "June 2023 - July 2023",
+    desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "GirlScript Summer of Code",
+    date: "May 2023 - Present",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+    doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+  },
+  {
+    id: 3,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+    role: "Android Developer",
+    company: "DSC KIIT",
+    date: "Nov2021 - Present",
+    desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+    skills: [
+      "Leadership",
+      "Mobile Application Development",
+      "Kotlin",
+      "XML",
+      "Figma",
+    ],
+  },
+  {
+    id: 4,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
+    role: "Android Developer Intern",
+    company: "Rudraksha Welffare Foundation",
+    date: "June 2021 - Oct 2021",
+    desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+    skills: [
+      "Android",
+      "Java",
+      "Kotlin",
+      "XML",
+      "Node Js",
+      "Cloud Firestore",
+      "Firebase",
+      "Figma",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "hacktiv8-logo.png",
+    school: "Hacktiv8",
+    date: "01/2024 - 04/2024",
+    grade: "94.00",
+    desc: "I have decided to transition my career to become a software engineer. To embark on this new journey, I have joined the Hacktiv8 bootcamp and enrolled in the Fullstack JavaScript program. This marks the beginning of my journey to become a developer.",
+    degree:
+      "FullStack Developer",
+  },
+  {
+    id: 1,
+    img: "grafika-logo.png",
+    school: "SMK Grafika, Yayasan Lektur",
+    date: "2020 - 2023",
+    grade: "91.0",
+    desc: "I successfully completed my vocational high school with a focus on the field of design and creativity as a multimedia student.",
+    degree: "Multimedia",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Portfolio",
+    description:
+      "Explore my sleek portfolio, built with HTML5, CSS3, JavaScript, and React.js. Discover my skills and projects through seamless design and interactivity, optimized for any device.",
+    image: "portofolio-logo.png",
+    tags: ["HTML5", "CSS3", "JavaScript", "React"],
+    category: "web app",
+    github: "https://github.com/kjcreddy666/portfolio",
+  },
+
+  {
+    id: 9,
+    title: "Eternal Tech Hire",
+    description: `"Eternal Tech Hire" is a web based platform to view available job fair events along with some companies participating in the events.`,
+    image: "ETH.jpeg",
+    tags: ["NextJS", "Tailwind CSS", "MongoDB", "TypeScript"],
+    category: "web app",
+    github: "https://github.com/Eternal-Tech-Hire/Final-Project",
+  },
+
+  {
+    id: 9,
+    title: "Pick A Pict",
+    description:
+      `"Pick A Pict" is a website based on social media to display some pictures that users can add to their collections.`,
+    image: "pick-a-pict.png",
+    tags: ["React", "JavaScript", "Firebase", "Tailwind CSS"],
+    category: "web app",
+    github: "https://pick-a-pict.web.app/",
+  },
+  {
+    id: 9,
+    title: "HackRigo",
+    description:
+      `The fashion e-commerce website captures Erigo design philosophy white catering to fashion enthusiasts unique needs.`,
+    image: "hackrigo.png",
+    tags: ["NextJS", "MongoDB", "TypeScript", "Tailwind CSS"],
+    category: "web app",
+    github: "https://hack-rigo.vercel.app/",
+  },
+  {
+    id: 9,
+    title: "Instagram (Clone)",
+    description:
+      `A mobile app based social media inspired by instagram.`,
+    image: "instagram.png",
+    tags: ["React Native", "GraphQL", "JavaScript", "Tailwind CSS"],
+    category: "web app",
+    github: "https://expo.dev/preview/update?message=Finish%20Update%20App&updateRuntimeVersion=1.0.0&createdAt=2024-03-31T14%3A58%3A58.148Z&slug=exp&projectId=615d5bef-7b61-4c89-a251-044716376bd4&group=1bdeffa3-f2b6-4ec0-850c-080ed454e9b3",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
